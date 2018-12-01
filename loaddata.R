a72c3e8d965a7b792d642420bd2cfaae34d3d66b
RemiDen <mrwemi@gmail.com>
2018-12-01 19:15:37 +0100

    Assignment wants only 4 R files - refactor

--- loaddata.R
+++ loaddata.R
@@ -1,5 +1,6 @@
 ## Assignemnt 1
 
+###################### Download zip
 zipurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 zipfile <- "power_consumption.zip"
 if (!file.exists(zipfile)) {
@@ -9,27 +10,22 @@ if (!file.exists(zipfile)) {
         unzip(zipfile)
 }
 
-# When loading the dataset into R, please consider the following:
-#         
-# The dataset has 2,075,259 rows and 9 columns.
-# 
-# First calculate a rough estimate of how much memory the dataset will require
-# in memory before reading into R. Make sure your computer has enough memory
-# (most modern computers should be fine).
-# 
-# We will only be using data from the dates 2007-02-01 and 2007-02-02.
+###################### Read downloaded file
 
 library(readr)
 library(dplyr)
 library(lubridate)
 
-dataset <- read_delim(file = "household_power_consumption.txt",
-                      col_names = TRUE,
-                      delim = ";")
-
-ds <- mutate(dataset, Date=dmy(Date)) %>%
-        filter(as.Date("2007-02-01") <= Date & Date <= as.Date("2007-02-02")) %>%
-        mutate(dtime=as.POSIXct(ymd(Date) + hms(Time)))
-
-rm(list = c("dataset"))
-
+loaddata <- function() {
+        dataset <- read_delim(file = "household_power_consumption.txt",
+                              col_names = TRUE,
+                              delim = ";")
+        
+        ds <- mutate(dataset, Date=dmy(Date)) %>%
+                filter(as.Date("2007-02-01") <= Date & Date <= as.Date("2007-02-02")) %>%
+                mutate(dtime=as.POSIXct(ymd(Date) + hms(Time)))
+        
+        rm(list = c("dataset"))
+        
+        ds
+}
--- plot3.R
+++ plot3.R
@@ -1,4 +1,31 @@
-if (! ("ds" %in% ls())) source("loaddata.R") ## `ds` is now available
+###################### Download zip
+zipurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipfile <- "power_consumption.zip"
+if (!file.exists(zipfile)) {
+        download.file(zipurl,
+                      dest = zipfile,
+                      meth = "curl");
+        unzip(zipfile)
+}
+
+###################### Read downloaded file
+
+library(readr)
+library(dplyr)
+library(lubridate)
+
+
+dataset <- read_delim(file = "household_power_consumption.txt",
+                      col_names = TRUE,
+                      delim = ";")
+
+ds <- mutate(dataset, Date=dmy(Date)) %>%
+        filter(as.Date("2007-02-01") <= Date & Date <= as.Date("2007-02-02")) %>%
+        mutate(dtime=as.POSIXct(ymd(Date) + hms(Time)))
+
+rm(list = c("dataset"))
+
+###################### Plot
 
 plot.png <- function(filename, fun) {
         png(filename = filename, width = 480, height = 480)
